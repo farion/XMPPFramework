9355b353d9dc8f86aa49f2cd6784bf20876511b0
Frieder Reinhold <reinhold@trigon-media.com>
2016-03-26 23:06:00 +0100

    initial XEP-0313 category test

---
+++ Extensions/XEP-0313/XMPPMessageArchiving+XEP_0313.h
@@ -0,0 +1,8 @@
+#import "XMPPMessageArchiving.h"
+
+@interface XMPPMessageArchiving (XEP_0313)
+
+-(void) query;
+
+@end
+
---
+++ Extensions/XEP-0313/XMPPMessageArchiving+XEP_0313.m
@@ -0,0 +1,9 @@
+#import "XMPPMessageArchiving+XEP_0313.h"
+
+@implementation XMPPMessageArchiving (XEP_0313)
+
+-(void) query {
+    NSLog(@"Test XMPPMessageArchiving query called.");
+}
+
+@end
